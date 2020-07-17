606c62373a2b72ae49e5899c212ed65fbe459a49
gyawalisaurav <saurav.gyawali@yahoo.com>
2020-07-17 12:39:01 -0400

    Json response

--- tests/socket_env.py
+++ tests/socket_env.py
@@ -2,7 +2,7 @@ import socket
 
 import gym
 import gym_novel_gridworlds
-
+import json
 
 env_id = 'NovelGridworld-v0'
 env = gym.make(env_id)
@@ -19,12 +19,13 @@ print('Connected with agent: ', addr)
 obs = env.reset()
 for i in range(50):
     action = conn_agent.recv(1024).decode()  # get action from agent
+    action = action[:-1]
     action = list(env.action_str.keys())[list(env.action_str.values()).index(action)]
 
     print("action: ", action, env.action_str[action])
     observation, reward, done, info = env.step(action)
-
-    conn_agent.sendall(str.encode(str(observation)))  # send observation to agent
+    msg = {'observation': observation.tolist(), 'reward': reward, 'done': done}
+    conn_agent.sendall(str.encode(json.dumps(msg)))  # send observation to agent
 
     env.render()
     print("Step: " + str(i) + ", reward: ", reward)
