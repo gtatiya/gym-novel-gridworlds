df626665c44a196104faa624c7f1926139e2a8b7
goelshivam1210 <goelshivam1210@gmail.com>
2021-02-02 12:24:53 -0500

    modified lidar in front wrapper to include inventory

--- gym_novel_gridworlds/observation_wrappers.py
+++ gym_novel_gridworlds/observation_wrappers.py
@@ -1,4 +1,5 @@
 import math
+import copy
 
 import numpy as np
 
@@ -8,60 +9,71 @@ from gym import error, spaces, utils
 
 class LidarInFront(gym.core.ObservationWrapper):
     """
-    Send several beans (self.num_beams) at equally spaced angles in 180 degrees in front of agent
+    Send several beans (self.num_beams) at equally spaced angles in 180 degrees in front of agent + agent's current inventory
     """
 
     def __init__(self, env, num_beams=5):
         super().__init__(env)
 
         # Observation Space
+        items_to_exclude = ['air', self.goal_item_to_craft]       
+        self.lidar_items = copy.deepcopy(self.env.items_id)
+        list(map(self.lidar_items.pop, items_to_exclude)) # remove air and final goal object from the list of lidar items
+        self.lidar_items_id = self.set_items_id(self.lidar_items) # set IDs for all the lidar items
         self.num_beams = num_beams
         self.max_beam_range = int(math.sqrt(2 * (self.env.map_size - 2) ** 2))  # Hypotenuse of a square
-        low = np.ones(len(self.env.items_id) * self.num_beams, dtype=int)
-        high = np.array([self.max_beam_range] * len(self.env.items_id) * self.num_beams)
+        low = np.zeros(len(self.lidar_items) * self.num_beams + len(self.inventory_items_quantity), dtype=int)
+        high = np.array([self.max_beam_range] * len(self.lidar_items) * self.num_beams + [20] * len(self.inventory_items_quantity)) # 20 denotes the maximum quantity of any item in the inventory
         self.observation_space = spaces.Box(low, high, dtype=int)
 
     def get_lidarSignal(self):
         """
-        Send several beans (self.num_beams) at equally spaced angles in front of agent
-        For each bean store distance (beam_range) for each item if item is found otherwise self.max_beam_range
+        Send several beams (self.num_beams) at equally spaced angles in 360 degrees in front of agent within a range
+        For each bean store distance (beam_range) for each item in items_id_lidar if item is found otherwise 0
         and return lidar_signals
         """
 
         direction_radian = {'NORTH': np.pi, 'SOUTH': 0, 'WEST': 3 * np.pi / 2, 'EAST': np.pi / 2}
 
-        # Shoot beams in 180 degrees in front of agent
-        angles_list = np.linspace(direction_radian[self.env.agent_facing_str] - np.pi / 2,
-                                  direction_radian[self.env.agent_facing_str] + np.pi / 2, self.num_beams)
+        # Shoot beams in 360 degrees in front of agent
+        angles_list = np.linspace(direction_radian[self.agent_facing_str] - np.pi,
+                                  direction_radian[self.agent_facing_str] + np.pi,
+                                  self.num_beams + 1)[:-1]  # 0 and 360 degree is same, so removing 360
 
         lidar_signals = []
-        r, c = self.env.agent_location
+        r, c = self.agent_location
         for angle in angles_list:
             x_ratio, y_ratio = np.round(np.cos(angle), 2), np.round((np.sin(angle)), 2)
-
-            beam_range = 1
-            # beam_signal = np.zeros(len(self.env.items_id), dtype=int)
-            beam_signal = np.full(fill_value=self.max_beam_range, shape=len(self.env.items_id), dtype=int)
+            beam_signal = np.zeros(len(self.lidar_items_id), dtype=int)
 
             # Keep sending longer beams until hit an object or wall
-            while True:
+            for beam_range in range(1, self.max_beam_range+1):
                 r_obj = r + np.round(beam_range * x_ratio)
                 c_obj = c + np.round(beam_range * y_ratio)
-                obj_id_rc = self.env.map[int(r_obj)][int(c_obj)]
+                obj_id_rc = self.map[int(r_obj)][int(c_obj)]
 
                 # If bean hit an object or wall
                 if obj_id_rc != 0:
-                    beam_signal[obj_id_rc - 1] = beam_range
+                    item = list(self.items_id.keys())[list(self.items_id.values()).index(obj_id_rc)]
+                    if item in self.lidar_items_id:
+                        obj_id_rc = self.lidar_items_id[item]
+                        beam_signal[obj_id_rc - 1] = beam_range
                     break
-
-                beam_range += 1
 
             lidar_signals.extend(beam_signal)
 
-        return np.array(lidar_signals)
+        return lidar_signals
 
     def observation(self, obs):
-        return self.get_lidarSignal()
+        """
+        observation is lidarSignal + inventory_items_quantity
+        :return: observation
+        """
+
+        lidar_signals = self.get_lidarSignal()
+        observation = lidar_signals + list(self.inventory_items_quantity.values())
+        
+        return np.array(observation)
 
 
 class AgentMap(gym.core.ObservationWrapper):
